fcc7f8c5dfc886bfb41d790924381010c0fe7815
Aditya <59937191+gamefan449@users.noreply.github.com>
2021-06-25 23:05:34 -0400

    Add files via upload
    main code to track hands

---
+++ main.py
@@ -0,0 +1,47 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+import random
+
+cap = cv.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+mpDraw = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.75,
+                       min_tracking_confidence=0.75)
+
+while True:
+    success, frame = cap.read()
+    frame = cv.flip(frame, 1)
+    rectangle = cv.rectangle(frame, (30, 30), (100, 100), (0, 255, 0), thickness=-1)
+    cv.putText(rectangle, "Catch me!", (30, 75), cv.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(0, 0, 255), thickness=2)
+
+    frameRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    #frameHeight, frameWidth, _ = frame.shape
+    result = hands.process(frameRGB)
+    hand_land_mark = result.multi_hand_landmarks  # hand land marks
+    if hand_land_mark:
+        for handLM in hand_land_mark:
+            for id, lm in enumerate(handLM.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+
+                if id == 8:
+                    cv.circle(frame, (cx, cy), 25, (255, 0, 255), -1)
+
+            # for point in mp_hands.HandLandmark:
+            mpDraw.draw_landmarks(frame, handLM, mp_hands.HAND_CONNECTIONS)
+            #     normalizedLandmark = handLM.landmark[point]  # Gets name of point
+            #     pixelCoordinatesLandmark = mpDraw._normalized_to_pixel_coordinates(normalizedLandmark.x,
+            #                                                                        normalizedLandmark.y, frameWidth,
+            #                                                                        frameHeight)
+
+            # print(point)
+            # print(pixelCoordinatesLandmark)
+            # print(normalizedLandmark)
+
+    cv.imshow("video", frame)
+
+    cv.waitKey(1)
