f045cdd6a5e234c60d90541d4a3988a2773115cc
anirband1 <deshmukh.anirban@gmail.com>
2021-11-13 22:26:44 +0530

    Experiment with shell script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Diagnostics;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
 
     [HideInInspector] public Vector2 screenBounds;
+
+    public string execPath;
 
     string appPath;
     void Awake()
@@ -17,6 +20,46 @@ public class GameManager : MonoBehaviour
     {
         appPath = Application.streamingAssetsPath + "/App.app"; // -- W A R N I N G -- check extension
 
+        execPath = ExecuteBashCommand("echo $PWD");
+
+        UnityEngine.Debug.Log(execPath);
         // Process.Start(appPath);
+    }
+
+    /*
+
+    void TestRun()
+    {
+        // lets say we want to run this command:    
+        //  t=$(echo 'this is a test'); echo "$t" | grep -o 'is a'
+        var output = ExecuteBashCommand("t=$(echo 'this is a test'); echo \"$t\" | grep -o 'is a'");
+
+        // output the result
+        Console.WriteLine(output);
+    }
+    */
+
+    static string ExecuteBashCommand(string command)
+    {
+        // according to: https://stackoverflow.com/a/15262019/637142
+        // thans to this we will pass everything as one command
+        command = command.Replace("\"", "\"\"");
+
+        var proc = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = "-c \"" + command + "\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = false
+            }
+        };
+
+        proc.Start();
+        proc.WaitForExit();
+
+        return proc.StandardOutput.ReadToEnd();
     }
 }
---
+++ Assets/StreamingAssets/ExecutePython.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo $PWD
+# cd Assets/StreamingAssets
+# ls
+
+# cd ..
+
+# ls
+# python3 main.py
