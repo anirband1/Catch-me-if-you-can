95f293d6ecc86cd58df039b6b5902a4de13d3c65
Aditya <59937191+Aditya-Tripuraneni@users.noreply.github.com>
2022-05-28 22:18:45 -0400

    Update main.py
    idk i was checking github and i think i accidently pushed something here, anyways, i put back the old code so all good

--- main.py
+++ main.py
@@ -1,132 +1,116 @@
-from cv2 import VideoCapture, flip, COLOR_BGR2RGB, cvtColor, circle, FILLED, destroyAllWindows, imshow
-import mediapipe.python.solutions.drawing_utils as mp_drawing
-import mediapipe.python.solutions.hands as mp_hands
-import pygame
-import math
-import pyautogui
-import os
+print("importing videocapture")
+from cv2 import VideoCapture  # Takes time to import
+print("Importing important cv functions ")
+from cv2 import flip, cvtColor, COLOR_BGR2RGB, circle, imshow, waitKey, putText, FONT_HERSHEY_COMPLEX, waitKey 
+
+print("importing mediapipe")
+import mediapipe.python.solutions.hands as hands_solution
+import mediapipe.python.solutions.drawing_utils as mp_draw_utils
+
+print("Importing other stuff...")
+import time
+import socket
+import sys
+
+# c# communication requirements
+connectionRefused = True
+quitApp = False
+
+try:
+    host, port = "localhost", 9999
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    connectionRefused = False
+except (ConnectionRefusedError):
+    pass
+
+# hand detection
+cap = VideoCapture(0)  # cv here
+
+mp_hands = hands_solution
+mp_draw = mp_draw_utils
+
+hands = mp_hands.Hands(static_image_mode=False,
+                       max_num_hands=2,
+                       min_detection_confidence=0.75,
+                       min_tracking_confidence=0.75)
+
+sPosVector = "0,0,0"
 
 
-WIDTH, HEIGHT = 640, 480
-
-# COLOURS
-RED = (255, 0, 0)
-VIOLET = (148, 0, 211)
-INDIGO = (75, 0, 130)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-YELLOW = (255, 255, 0)
-ORANGE = (255, 127, 0)
-WHITE = (255, 255, 255)
-
-color = RED
-rainbow = [VIOLET, INDIGO, BLUE, GREEN, YELLOW, ORANGE, RED]
-
-desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-ss_count = 1
-
-run = True
-
-pygame.init()
-
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Drawing Board")
-
-hands = mp_hands.Hands(max_num_hands=1, min_tracking_confidence=0.9)
-camera = VideoCapture(0)
+start_time = 0 
+fps = 0 
 
 
-def generate_rainbow(rainbow):
-    it = iter(rainbow)
-    while True:
-        yield next(it)
-        try:
-            yield next(it)
-        except StopIteration:
-            it = iter(rainbow)
-
-
-def convert_to_pixel_coordinates():
-    normalized_landmark = handlms.landmark[id]
-    pixel_coordinate = mp_drawing._normalized_to_pixel_coordinates(normalized_landmark.x,
-                                                                   normalized_landmark.y, width, height)
-
-    return pixel_coordinate
-
-
-gen_rainbow = generate_rainbow(rainbow)
-window.fill(WHITE)
-
-
+run = True 
 while run:
-    ret, frame = camera.read()
-    frame = flip(frame, 1)
-    imgRGB = cvtColor(frame, COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    hand = results.multi_hand_landmarks
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_q]:
-        run = False
-    if keys[pygame.K_c]:
-        window.fill(WHITE)
-    if keys[pygame.K_r]:
-        color = RED
-    if keys[pygame.K_b]:
-        color = BLUE
-    if keys[pygame.K_y]:
-        color = YELLOW
-    if keys[pygame.K_g]:
-        color = GREEN
-    if keys[pygame.K_o]:
-        color = ORANGE
-    if keys[pygame.K_l]:
-        color = next(gen_rainbow)
+    if quitApp:
+        break
 
-    if hand:
-        for handlms in hand:
-            for id, lm in enumerate(handlms.landmark):
-                height, width, c = frame.shape
-                cx, cy = int(lm.x * width), int(lm.y * height)
-                if id == 4:
-                    circle(frame, (cx, cy), 15, (255, 0, 0), FILLED)
-                    thumb_coordinates = convert_to_pixel_coordinates()
+    time.sleep(1 / 60)  # Rough value of Time.deltaTime in unity
 
-                if id == 8:
-                    circle(frame, (cx, cy), 15, (255, 0, 255), FILLED)
-                    index_coordinates = convert_to_pixel_coordinates()
+    success, frame = cap.read()
+    frame = flip(frame, 1)  # cv here
 
-                    x_distance = index_coordinates[0] - thumb_coordinates[0]
-                    y_distance = index_coordinates[1] - thumb_coordinates[1]
-                    distance = math.sqrt((x_distance ** 2) + (y_distance ** 2))
+    # key = cv.waitKey(1) & 0xFF  # Keyboard input
+    end_time = time.time()
+    delta_time = end_time - start_time
+    fps = 1/delta_time
+    start_time = end_time
 
-                    # try:
-                    if 0 <= distance <= 20:
-                        pygame.draw.circle(window, color,
-                                           (index_coordinates[0], index_coordinates[1]), 5)
+    putText(frame, f"FPS: {int(fps)}", (5,30), FONT_HERSHEY_COMPLEX, 1, (255,255,255))
 
-                if id == 12:
-                    circle(frame, (cx, cy), 15, (255, 255, 0), FILLED)
-                    middle_tip_coordinates = convert_to_pixel_coordinates()
+    frameRGB = cvtColor(frame, COLOR_BGR2RGB)  # cv here
+    #frameHeight, frameWidth, _ = frame.shape
+    result = hands.process(frameRGB)
+    hand_land_mark = result.multi_hand_landmarks  # hand land marks
 
-                    x_middle_distance = index_coordinates[0] - middle_tip_coordinates[0]
-                    y_middle_distance = index_coordinates[1] - middle_tip_coordinates[1]
-                    distance_middle_to_index = distance = math.sqrt((x_middle_distance ** 2) + (y_middle_distance ** 2))
+    #lets user quit from cv window 
+    if waitKey(1) & 0xFF == ord('q'):
+        print("Quitting...")
+        run = False 
 
-                    if 0 <= distance_middle_to_index <= 20:
-                        print(desktop)
-                        image = pyautogui.screenshot()
-                        image.save(desktop + f"/screenshot{ss_count}.png")
-                        ss_count += 1
+    if hand_land_mark:  # hand on screen
+        for handLM in hand_land_mark:
+            for id, lm in enumerate(handLM.landmark):
 
-                mp_drawing.draw_landmarks(frame, handlms, mp_hands.HAND_CONNECTIONS)
+                if id == 8:  # index finger
 
-    pygame.display.update()
-    imshow("Window", frame)
+                    h, w, c = frame.shape
+                    # cx, cy = int(lm.x * w), int(lm.y * h)
 
-camera.release()
-destroyAllWindows()
-pygame.quit()
+                    cx, cy = (lm.x), (lm.y)
+                    # print(id, cx, cy)
+
+                    circle(frame, (int(cx * w), int(cy * h)), 25,
+                           (255, 0, 255), -1)
+
+                    sPosVector = f"{2 * cx - 1},{2 * cy - 1},0"  # position of index finger (scaled to be from -1 to 1)
+
+            mp_draw.draw_landmarks(frame, handLM, mp_hands.HAND_CONNECTIONS)
+
+    # Converting string to Byte, and sending it to C#
+        #testing
+    if not connectionRefused:
+        sock.sendall(sPosVector.encode("UTF-8"))
+
+        #receiveing data in Byte fron C#, and converting it to String
+        receivedData = sock.recv(1024).decode(
+            "UTF-8"
+        )  # change 1024 to something lower hopefully makes things faster (128 ?)
+
+        if receivedData == "Stop":
+            quitApp = True
+
+    imshow("video", frame)
+
+    waitKey(1)
+
+sock.close()
+sys.exit()
+
+# put connect block in func, every x milliseconds, call func until connects ?
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+#     sock.connect((host, port))
